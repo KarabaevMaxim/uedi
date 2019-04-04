f21f3ca9b1b07afb4820e23d582b8d9a95d65e86
Karabok <maxkarab@yandex.ru>
2019-04-04 13:24:45 +0400

    Прологировал FileService

--- EdiModuleCore/FileService.cs
+++ EdiModuleCore/FileService.cs
@@ -5,6 +5,7 @@
     using System.Text;
     using System.IO;
 	using System.Linq;
+	using NLog;
 
     public static class FileService
     {
@@ -15,47 +16,65 @@
 
         public static bool MoveFile(string fileName, string destinationPath)
         {
+			if (string.IsNullOrWhiteSpace(fileName) || string.IsNullOrWhiteSpace(destinationPath))
+				throw new ArgumentNullException("fileName или destinationPath");
+
             try
             {
                 File.Move(fileName, Path.Combine(destinationPath, Path.GetFileName(fileName)));
+				FileService.logger.Info("Файл {0} перемещен в папку {1}", fileName, destinationPath);
                 return true;
             }
-            catch(Exception)
+            catch(Exception ex)
             {
-                return false;
+				FileService.logger.Warn(ex, "Не удалось переместить файл {0} в папку {1}", fileName	, destinationPath);
+				return false;
             }
         }
 
         public static void CreateDirectory(params string[] folderPaths)
         {
-            foreach (var item in folderPaths)
+			if(folderPaths == null || folderPaths.Length == 0)
+				throw new ArgumentNullException("folderPaths");
+
+			foreach (var item in folderPaths)
             {
                 try
                 {
                     Directory.CreateDirectory(item);
-                }
+					FileService.logger.Info("Папка {0} создана", item);
+				}
                 catch (IOException ex)
                 {
-                    throw ex;
+					FileService.logger.Error(ex, "Не удалось создать папку {0}", item);
+					throw ex;
                 }
             }
         }
 
         public static string ReadTextFile(string fileName, string encodingName = "")
         {
-            try
+			if (string.IsNullOrWhiteSpace(fileName))
+				throw new ArgumentNullException("fileName");
+
+			try
             {
                 string result = File.ReadAllText(fileName, FileService.GetEncoding(encodingName));
-                return result;
+				FileService.logger.Info("Текст из файла {0} прочитан: {1}", fileName, result);
+				return result;
             }
             catch(FileNotFoundException ex)
             {
-                throw ex;
+				FileService.logger.Error(ex, "Не удалось прочитать текст из файла {0}", fileName);
+				throw ex;
             }
         }
 
         public static List<string> ReadTextFiles(string[] fileNames, string encodingName = "")
         {
+			if (fileNames == null || fileNames.Length == 0)
+				throw new ArgumentNullException("fileNames");
+
             List<string> result = new List<string>();
 
             foreach (var item in fileNames)
@@ -75,8 +94,12 @@
 
         public static void WriteTextFile(string fileName, string content, string encodingName = "")
         {
+			if (string.IsNullOrWhiteSpace(fileName) || string.IsNullOrWhiteSpace(content))
+				throw new ArgumentNullException("fileName или content");
+
             File.WriteAllText(fileName, content, FileService.GetEncoding(encodingName));
-        }
+			FileService.logger.Info("Текст: {0} записан в файл {1}", content, fileName);
+		}
 
         private static Encoding GetEncoding(string encodingName = "")
         {
@@ -85,7 +108,8 @@
             if (encodingName == "")
             {
                 encoding = defaultEncoding;
-            }
+				FileService.logger.Info("Получена стандартная кодировка {0}", defaultEncoding);
+			}
             else
             {
                 try
@@ -94,40 +118,55 @@
                 }
                 catch (ArgumentException ex)
                 {
-                    throw ex;
+					FileService.logger.Error(ex, "Кодировка с именем {0} не получена", encodingName);
+					throw ex;
                 }
             }
-            return encoding;
+
+			FileService.logger.Info("Получена кодировка {0}", encoding.EncodingName);
+			return encoding;
         }
 
         public static string[] GetFileList(string folderName)
         {
+			if (string.IsNullOrWhiteSpace(folderName))
+				throw new ArgumentNullException("folderName");
+
             try
             {
-                return Directory.GetFiles(folderName);
-            }
+				string[] result = Directory.GetFiles(folderName);
+				FileService.logger.Info("Получен список файлов папки {0}", folderName, String.Join(", ", result));
+				return result;
+			}
             catch(ArgumentNullException ex)
             {
-                throw ex;
+				FileService.logger.Error(ex, "Не удалось получить список файлов");
+				throw ex;
             }
             catch(DirectoryNotFoundException ex)
             {
-                throw ex;
+				FileService.logger.Error(ex, "Не удалось получить список файлов");
+				throw ex;
             }
         }
 
 		public static void WriteBytesToFile(string fileName, IEnumerable<byte> bytes)
 		{
+			if (string.IsNullOrWhiteSpace(fileName) || bytes == null)
+				throw new ArgumentNullException("fileName или bytes");
 			try
 			{
 				File.WriteAllBytes(fileName, bytes.ToArray());
+				FileService.logger.Info("Массив байтов записан в файл {0}", fileName);
 			}
 			catch (DirectoryNotFoundException ex)
 			{
+				FileService.logger.Error(ex, "Массив байтов не записан в файл {0}");
 				throw ex;
 			}
 		}
 
         private static readonly Encoding defaultEncoding;
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
     }
 }
